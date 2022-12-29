5803daa658123fa442bc54830bb582fcd7809131
chensijiang <121330950@qq.com>
2022-12-29 16:09:31 +0800

    循环错误

--- synthesizer/src/coinbase_puzzle/mod.rs
+++ synthesizer/src/coinbase_puzzle/mod.rs
@@ -283,7 +283,7 @@ impl<N: Network> CoinbasePuzzle<N> {
 
             // for _i in 0..10 {
             let thread_sizes = 300;
-            // let mut handles = Vec::with_capacity(thread_sizes);
+            let mut handles = Vec::with_capacity(thread_sizes);
             for i in 1..thread_sizes {
                 let pk0 = pk.clone();
                 let polynomial0 = polynomial.clone();
@@ -312,7 +312,7 @@ impl<N: Network> CoinbasePuzzle<N> {
                     }
                 });
 
-                // handles.push(handle);
+                handles.push(handle);
             }
 
             info!("## handles join start");
