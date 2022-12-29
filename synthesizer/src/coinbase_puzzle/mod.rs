bcf06985f9c5af51645d12eafab1046a997e7677
chensijiang <121330950@qq.com>
2022-12-29 15:11:10 +0800

    尝试死循环一直运行

--- synthesizer/src/coinbase_puzzle/mod.rs
+++ synthesizer/src/coinbase_puzzle/mod.rs
@@ -260,7 +260,7 @@ impl<N: Network> CoinbasePuzzle<N> {
             let handle = std::thread::spawn(move || {
 
 
-                for iii in 0..10 {
+                loop {
                     let product_evaluations = {
 
                         let polynomial_evaluations = pk0.product_domain.in_order_fft_with_pc(&polynomial0, &pk0.fft_precomputation);
@@ -273,19 +273,19 @@ impl<N: Network> CoinbasePuzzle<N> {
                         product_evaluations
                     };
                     pe_tx0.send(product_evaluations).unwrap();
-                    info!("### pe_tx0 send iii={}",iii);
+                    info!("### pe_tx0 send" );
                 }
 
 
             });
             pe_handles.push(handle);
         }
-        // pe_run_flag = false;
-        info!("### wait pe_handles");
-        for handle in pe_handles {
-            handle.join().unwrap();
-        }
-        info!("### wait pe_handles ok ");
+        // // pe_run_flag = false;
+        // info!("### wait pe_handles");
+        // for handle in pe_handles {
+        //     handle.join().unwrap();
+        // }
+        // info!("### wait pe_handles ok ");
 
         let mut rets = Vec::<ProverSolution<N>>::new();
 
@@ -308,14 +308,16 @@ impl<N: Network> CoinbasePuzzle<N> {
                 //         s
                 //     }
                 // };
-                info!("### pe_rx recv begin " );
-                let product_evaluations = pe_rx.recv().unwrap();
-                info!("### pe_rx recv end " );
-                let product_evaluations0 = product_evaluations.clone();
 
                 let handle = std::thread::spawn(move || {
-                    let ret = prove_ex_inner(&pk0, &polynomial0, &epoch_challenge0, &address0, nonce0, minimum_proof_target0, &product_evaluations0);
-                    ret
+                   loop {
+                       info!("### pe_rx recv begin " );
+                       let product_evaluations = pe_rx.recv().unwrap();
+                       info!("### pe_rx recv end " );
+                       let product_evaluations0 = product_evaluations.clone();
+                       let ret = prove_ex_inner(&pk0, &polynomial0, &epoch_challenge0, &address0, nonce0, minimum_proof_target0, &product_evaluations0);
+                       ret
+                   }
                 });
 
                 handles.push(handle);
